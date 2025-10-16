494cad5ffe4638be4fc5e15992f2617b356e5115
SOLO Estate <info@solo-estate.com>
2025-10-17 00:17:10 +0400

    Update layout.tsx

--- app/studio/[[...tool]]/layout.tsx
+++ app/studio/[[...tool]]/layout.tsx
@@ -8,5 +8,9 @@ export default function StudioLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  )
 }
